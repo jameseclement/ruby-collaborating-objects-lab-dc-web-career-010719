f9fa50a4dcc840250181f87771dead8fe787d2de
James Clement <jameseclement@gmail.com>
2019-01-11 16:33:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,33 @@
+require 'pry'
+
 class Artist
   
   attr_accessor :name, :songs
+  
+  
+  @@all = []
+  
+  def self.all 
+    @@all 
+  end
   
   def initialize(name)
   @name = name
   @songs = []
   end
 
-  def add_song (song_name)
-   song = Song.new(song_name)
-   
+  def add_song (song)
    self.songs << song
-   
-   
-  
-  end
+   end
 
-  def find_artist
-  
+  def save
+    @@all << self
   end
   
+  def self.find_or_create_by_name(artist_name)
+    
+    
+    
+  end
   
 end
--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,9 @@ class Song
   
   attr_accessor :name, :artist
   
-  def initialize (name)
+  def initialize(name)
   @name = name
   end
-  
+ 
   
 end
